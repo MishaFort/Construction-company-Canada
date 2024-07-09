56d7f69f1d98dff2133bb6fb918d92deaa524ed7
MishaFort <fortunenko2011@yahoo.com>
2024-07-09 14:59:25 +0300

    almost done the alpha on home page 07.09.2024

---
+++ js/footer-form-validator.js
@@ -0,0 +1,37 @@
+/* const modal = document.querySelector('.backdrop'); */
+
+const validator = new JustValidate('#footer-form');
+validator
+  .addField(
+    '#user-email',
+    [
+      { rule: 'required', errorMessage: '&#8613; The field is required' },
+      { rule: 'email', errorMessage: '&#8613; Invalid field' },
+    ],
+    {
+      successMessage: 'Email looks fine!',
+    }
+  )
+
+  .addField(
+    '#terms-conditions',
+    [{ rule: 'required', errorMessage: '&#8613; The field is required' }],
+    {
+      errorMessage: '&#8613; Invalid field',
+    }
+  )
+
+  .onSuccess(event => {
+    location.reload();
+    /* modal.classList.toggle('is-hidden');
+
+    emailjs.sendForm('service_pnqld1g', 'contact_form', '#contact-form').then(
+      () => {
+        console.log('SUCCESS!');
+        window.location.reload();
+      },
+      error => {
+        console.log('FAILED...', error);
+      }
+    ); */
+  });
