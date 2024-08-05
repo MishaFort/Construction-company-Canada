f102c4b467183b87c0e4906172a855598de5654b
MishaFort <fortunenko2011@yahoo.com>
2024-08-05 12:40:02 +0300

    quick fix

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -26,7 +26,7 @@ const swiper = new Swiper('.swiper-container', {
 
   autoplay: {
     delay: 5000, // auto-slide every 5 seconds
-    disableOnInteraction: true, // pause autoplay when user interacts with the slider
+    // disableOnInteraction: true, // pause autoplay when user interacts with the slider
   },
   navigation: {
     nextEl: '.carousel-button.next',
